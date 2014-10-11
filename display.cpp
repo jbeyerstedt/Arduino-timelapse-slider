43995bb5acfebdcf3032036e723f80d2b8584b01
jtByt.Pictures <jtByt.Pictures@gmail.com>
2014-10-11 23:43:18 +0200

    little correction of display symbol constant
    there was a mistake in the description of the character S.

--- display.cpp
+++ display.cpp
@@ -22,7 +22,7 @@ const int WAIT_INPT = ((d1[3]|D1_DP)<<8) | (d2[3]|D2_DP); // d+DP | d+DP
 const int WAIT_IDLE = ((d1[1]|d1[2])<<8) | (d2[1]|d2[2]|d2[3]|d2[4]|d2[6]); // b+c | b+c+d+e+g
 
 const int MODE_IN   = ((d1[1]|d1[2])<<8) | (d2[2]|d2[4]|d2[6]); // b+c | c+e+g
-const int MODE_SL   = ((d1[0]|d1[1]|d1[4]|d1[5]|d1[6])<<8) | (d2[3]|d2[4]|d2[5]);; // a+b+d+f+g | d+e+f
+const int MODE_SL   = ((d1[0]|d1[2]|d1[3]|d1[5]|d1[6])<<8) | (d2[3]|d2[4]|d2[5]);; // a+c+d+f+g | d+e+f
 const int MODE_CO   = ((d1[3]|d1[4]|d1[6])<<8) | (d2[2]|d2[3]|d2[4]|d2[6]);; // d+e+g | c+d+e+g
 
 const int SET_TIME  = ((d1[1]|d1[2]|d1[3]|d1[4]|d1[6])<<8) | (d2[4]|d2[6]);; // b+c+d+e+g | e+g
